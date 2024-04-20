80a9ca104e64676a712417ab62898793a004d13c
PaulaCR90 <p.a.castrorodriguez@gmail.com>
2024-04-20 10:06:00 +0200

    debugg SimonSays and edit url for production

--- src/components/handleSimon/handleSimon.js
+++ src/components/handleSimon/handleSimon.js
@@ -5,6 +5,12 @@ import { WrongSequenceModal } from '../WrongSequenceModal/WrongSequenceModal';
 
 let sequencesEqual = 0;
 export const handleClick = (e, sequences, selectedColors) => {
+  const colorImage = e;
+  console.log(colorImage);
+  setTimeout(() => {
+    colorImage.style.width = '90%';
+  }, 0);
+
   const colorValue = e.target.getAttribute('data-value');
   selectedColors.push(colorValue);
   console.log(selectedColors);
